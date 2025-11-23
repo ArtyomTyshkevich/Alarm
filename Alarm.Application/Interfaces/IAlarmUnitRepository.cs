4f7021f14274ee488935aa64aaac20b687a094b5
ArtyomTyshkevich <frod_1999@mail.ru>
2025-11-23 17:16:40 +0300

    add AlarmUnitRepository

---
+++ Alarm.Application/Interfaces/IAlarmUnitRepository.cs
@@ -0,0 +1,15 @@
+﻿using Alarm.Core.Models;
+using Alarm.Infrastructure.Repositories;
+
+namespace Alarm.Application.Interfaces
+{
+    public interface IAlarmUnitRepository
+    {
+        Task AddAsync(AlarmUnit alarm);
+        Task DeleteAsync(Guid id);
+        Task<List<AlarmUnit>> GetAllAsync();
+        Task<AlarmUnit> GetByIdAsync(Guid id);
+        Task SaveChangesAsync();
+        Task UpdateAsync(AlarmUnit alarm);
+    }
+}
---
+++ Alarm.Infrastructure/Repositories/AlarmUnitRepository.cs
@@ -0,0 +1,53 @@
+﻿using Alarm.Application.Interfaces;
+using Alarm.Core.Models;
+using Alarm.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alarm.Infrastructure.Repositories
+{
+    public class AlarmUnitRepository : IAlarmUnitRepository
+    {
+        private readonly AlarmLocalDbContext _dbContext;
+
+        public AlarmUnitRepository(AlarmLocalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<AlarmUnit>> GetAllAsync()
+        {
+            return await _dbContext.Alarm.ToListAsync();
+        }
+
+        public async Task<AlarmUnit> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Alarm.FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public Task AddAsync(AlarmUnit alarm)
+        {
+            _dbContext.Alarm.Add(alarm);
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateAsync(AlarmUnit alarm)
+        {
+            _dbContext.Alarm.Update(alarm);
+            return Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var alarm = await _dbContext.Alarm.FirstOrDefaultAsync(a => a.Id == id);
+            if (alarm != null)
+            {
+                _dbContext.Alarm.Remove(alarm);
+            }
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+    }
+}
